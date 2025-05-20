31c9d5107dace9a54c98dcfd9d1ad2ac41b82a72
GerberCanahuiUMG <gerbercanahui39@gmail.com>
2025-05-19 22:19:37 -0600

    Form menu de Gerber

---
+++ Proyecto_Prueba/FormGerber.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Proyecto_Prueba
+{
+    partial class FormGerber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboxEstado = new System.Windows.Forms.ComboBox();
+            this.cboxUsuarioSistema = new System.Windows.Forms.ComboBox();
+            this.txtIngredientes = new System.Windows.Forms.TextBox();
+            this.cboxCategoria = new System.Windows.Forms.ComboBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigoMenu = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFechaActual = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.dgvMenus = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboxEstado);
+            this.groupBox1.Controls.Add(this.cboxUsuarioSistema);
+            this.groupBox1.Controls.Add(this.txtIngredientes);
+            this.groupBox1.Controls.Add(this.cboxCategoria);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtCodigoMenu);
+            this.groupBox1.Controls.Add(this.lblPrecio);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(40, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(934, 152);
+            this.groupBox1.TabIndex = 29;
+            this.groupBox1.TabStop = false;
+            // 
+            // cboxEstado
+            // 
+            this.cboxEstado.FormattingEnabled = true;
+            this.cboxEstado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo",
+            "Especial",
+            "Temporada"});
+            this.cboxEstado.Location = new System.Drawing.Point(501, 56);
+            this.cboxEstado.Name = "cboxEstado";
+            this.cboxEstado.Size = new System.Drawing.Size(124, 21);
+            this.cboxEstado.TabIndex = 33;
+            // 
+            // cboxUsuarioSistema
+            // 
+            this.cboxUsuarioSistema.FormattingEnabled = true;
+            this.cboxUsuarioSistema.Items.AddRange(new object[] {
+            "Augusto Morán",
+            "Gerber Canahuí",
+            "Fernando Fajardo"});
+            this.cboxUsuarioSistema.Location = new System.Drawing.Point(501, 26);
+            this.cboxUsuarioSistema.Name = "cboxUsuarioSistema";
+            this.cboxUsuarioSistema.Size = new System.Drawing.Size(124, 21);
+            this.cboxUsuarioSistema.TabIndex = 32;
+            // 
+            // txtIngredientes
+            // 
+            this.txtIngredientes.Location = new System.Drawing.Point(163, 85);
+            this.txtIngredientes.Name = "txtIngredientes";
+            this.txtIngredientes.Size = new System.Drawing.Size(124, 20);
+            this.txtIngredientes.TabIndex = 31;
+            // 
+            // cboxCategoria
+            // 
+            this.cboxCategoria.FormattingEnabled = true;
+            this.cboxCategoria.Items.AddRange(new object[] {
+            "Desayunos",
+            "Almuerzos",
+            "Cenas",
+            "Postres",
+            "Bebidas"});
+            this.cboxCategoria.Location = new System.Drawing.Point(163, 113);
+            this.cboxCategoria.Name = "cboxCategoria";
+            this.cboxCategoria.Size = new System.Drawing.Size(124, 21);
+            this.cboxCategoria.TabIndex = 19;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(163, 57);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(124, 20);
+            this.txtNombre.TabIndex = 17;
+            // 
+            // txtCodigoMenu
+            // 
+            this.txtCodigoMenu.Location = new System.Drawing.Point(163, 26);
+            this.txtCodigoMenu.Name = "txtCodigoMenu";
+            this.txtCodigoMenu.ReadOnly = true;
+            this.txtCodigoMenu.Size = new System.Drawing.Size(124, 20);
+            this.txtCodigoMenu.TabIndex = 16;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(547, 84);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(15, 19);
+            this.lblPrecio.TabIndex = 11;
+            this.lblPrecio.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(370, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 19);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Precio";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(370, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 19);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Estado";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(370, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 19);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Usuario Sistema";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Categoría";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingredientes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Codigo Menu";
+            // 
+            // lblFechaActual
+            // 
+            this.lblFechaActual.AutoSize = true;
+            this.lblFechaActual.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaActual.Location = new System.Drawing.Point(857, 65);
+            this.lblFechaActual.Name = "lblFechaActual";
+            this.lblFechaActual.Size = new System.Drawing.Size(15, 19);
+            this.lblFechaActual.TabIndex = 32;
+            this.lblFechaActual.Text = "-";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(749, 66);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(88, 19);
+            this.label14.TabIndex = 30;
+            this.label14.Text = "Fecha actual:";
+            // 
+            // dgvMenus
+            // 
+            this.dgvMenus.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvMenus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMenus.Location = new System.Drawing.Point(41, 272);
+            this.dgvMenus.Name = "dgvMenus";
+            this.dgvMenus.ReadOnly = true;
+            this.dgvMenus.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvMenus.Size = new System.Drawing.Size(933, 236);
+            this.dgvMenus.TabIndex = 31;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(455, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 24);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "MENUS";
+            // 
+            // FormGerber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 573);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblFechaActual);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.dgvMenus);
+            this.Controls.Add(this.label1);
+            this.Name = "FormGerber";
+            this.Text = "FormGerber";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cboxEstado;
+        private System.Windows.Forms.ComboBox cboxUsuarioSistema;
+        private System.Windows.Forms.TextBox txtIngredientes;
+        private System.Windows.Forms.ComboBox cboxCategoria;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigoMenu;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFechaActual;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.DataGridView dgvMenus;
+        private System.Windows.Forms.Label label1;
+    }
+}
